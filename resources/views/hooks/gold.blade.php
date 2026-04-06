149eb79c0ce560f4ae63a6f07f11d5a6bea59e3f
vicky <pratamavr@gmail.com>
2026-04-06 08:28:44 +0800

    updates

--- resources/views/hooks/gold.blade.php
+++ resources/views/hooks/gold.blade.php
@@ -1,5 +1,5 @@
 <div class="col-4 col-md-2 mb-2">
-  <a href="{{ route('apps.gold-prices') }}" class="menu-item rounded-4 p-2 disabled" disabled="true">
+  <a href="{{ route('apps.gold-prices') }}" class="menu-item rounded-4 p-2 disabled">
     <i class="bi bi-gem"></i>
     <span>Gold</span>
   </a>
